deed50846b5a20411350bf3d71e5e482b106d95a
David de la Varga <davidelavarga@gmail.com>
2021-03-07 19:35:52 +0100

    GPIO.setmode module level

--- hub/adapters/led_feeder_job_adapter.py
+++ hub/adapters/led_feeder_job_adapter.py
@@ -2,11 +2,10 @@ from hub.domain.ports import FeederJobPort
 import RPi.GPIO as GPIO
 import time
 
-class LedFeederJob(FeederJobPort):
-    def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(7, GPIO.OUT)
-    
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+class LedFeederJob(FeederJobPort):    
     def serve_portion(self, feeder_id: int):
         GPIO.output(7, True)
         time.sleep(1)
