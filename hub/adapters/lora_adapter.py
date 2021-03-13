93adf23f76256986451be79bf59d9761e92e7c63
David de la Varga <davidelavarga@gmail.com>
2021-03-13 21:14:15 +0100

    Leds configured

--- hub/adapters/lora_adapter.py
+++ hub/adapters/lora_adapter.py
@@ -1,5 +1,7 @@
 import logging
+import time
 
+import RPi.GPIO as GPIO
 from hub.domain.ports import FeederJobPort
 from utils.get_config import get_config
 
@@ -10,6 +12,7 @@ class LoraFeederJob(FeederJobPort):
         import board
         import busio
         from digitalio import DigitalInOut
+
         cs = DigitalInOut(board.CE1)
         reset = DigitalInOut(board.D25)
         spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
@@ -18,11 +21,42 @@ class LoraFeederJob(FeederJobPort):
         self.rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, self.__config["frecuency"])
         self.serve_action = self.__actions["serve"]
 
+        self.success_led = 13
+        self.fail_led = 26
+        self.warn_led = 19
+        self.__setup_check_leds()
+
     def serve_portion(self, feeder_id: int):
         logging.info(f"Post to {feeder_id}")
         try:
+            GPIO.output(self.success_led, True)
             self.rfm9x.send(bytes(f"{feeder_id} {self.serve_action}", "utf-8"))
-            logging.info(f"Successfully!")
+            logging.info(f"Sent LoRa message successfully!")
+            GPIO.output(self.success_led, False)
 
         except Exception:
+            GPIO.output(self.fail_led, True)
             logging.exception("Error sending serve job!")
+            time.sleep(2)
+            GPIO.output(self.fail_led, False)
+
+    def __setup_check_leds(self):
+        # Setup
+        GPIO.setup(self.success_led, GPIO.OUT)
+        GPIO.setup(self.fail_led, GPIO.OUT)
+        GPIO.setup(self.warn_led, GPIO.OUT)
+
+        # Off all
+        self.__set_leds_statues(False)
+
+        # On all
+        self.__set_leds_statues(True)
+        time.sleep(2)
+
+        # Off all
+        self.__set_leds_statues(False)
+    
+    def __set_leds_statues(self, status: bool):
+        GPIO.output(self.success_led, status)
+        GPIO.output(self.fail_led, status)
+        GPIO.output(self.warn_led, status)
